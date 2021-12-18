b62996c7118e620942754cd7af22e6c1ddf949a5
thatojadezweni <thatojadezweni@gmail.com>
2021-12-18 03:10:04 +0200

    set up basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello');
+import "reflect-metadata";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+if (!("PORT" in process.env)) throw new Error("PORT environment varianle is missing");
+
+const PORT = process.env.PORT || 8081;
+
+const app = express()
+
+app.listen(Number(PORT), () => {
+    console.log("[server]: Listening on port")
+})
